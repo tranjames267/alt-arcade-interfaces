b231aa9be4e6d26f80fff3e812637886cffc3e67
tranjames267 <58448725+tranjames267@users.noreply.github.com>
2023-09-20 21:59:02 -0600

    Create improvedSketch.js

---
+++ altPong/improvedSketch.js
@@ -0,0 +1,288 @@
+// ball color toggle
+var ballColor = 255; 
+var revealBall = false;
+var ballTimer = 0;
+
+// paddle color toggle
+var paddleColor = 255;
+var revealPaddle = false; 
+var paddleTimer = 0;
+
+// paddle noises
+var paddleMovingUp = false;
+var paddleMovingDown = false; 
+
+// playing the game?
+var isPlayingGame = 0; 
+
+// ball variables
+var ballX;
+var ballY;
+var ballWidth = 12;
+var ballHeight = 12;
+
+var ballXSpeed = 2;
+var ballYSpeed = 2; 
+var ballDirectionX = 1; // 1 is right, -1 is left
+var ballDirectionY = 1; // -1 is up, 1 is down
+
+// player variables
+var paddle1X = 15;
+var paddle1Y = 250;
+
+var paddle2X = 735;
+var paddle2Y = 250; 
+
+var paddleWidth = 10;
+var paddleHeight = 80; 
+var playerSpeed = 3;
+
+p1Score = 0;
+p2Score = 0; 
+
+function setup() {
+  createCanvas(750, 500);
+  background(0,0,0);
+  rectMode(CENTER);
+  textAlign(CENTER); 
+  ballX = width/2;
+  ballY = height/2; 
+}
+
+function preload() {
+  // from the mobile game Color Switch
+  goalNoise = loadSound('goalNoise.mp3');
+  song = loadSound('ColorSwitchTower.mp3');
+  wallHit = loadSound('wallHit.mp3');
+  paddleHit = loadSound('paddleHit.mp3');
+  lightOn = loadSound('lightOn.mp3');
+  lightOff = loadSound('lightOff.mp3');
+	paddleMoving = loadSound('laser.mp3');
+}
+
+function draw() {
+  keyPressed();
+  
+  background(0,0,0,30);
+  
+  song.setVolume(0.5);
+  if (!song.isPlaying()){ 
+    song.play(); 
+  }
+  if(isPlayingGame == 0){
+    noStroke(); 
+    fill(255); 
+    textSize(110); 
+     
+    textFont('Helvetica');
+    text('PONG', width/2, 190); 
+    
+    textSize(25);
+    text('Press ENTER to start, SHIFT to reset',width/2, 300);
+    text('Press A to light the paddles, S for the ball',width/2, 350); 
+    text('O moves the paddle up, L moves it down', width/2, 400);
+    
+    noFill();
+    strokeWeight(10);
+    stroke(255); 
+    rect(width/2, height/2, width-50, height-50); 
+    
+    
+  }
+  
+  if(isPlayingGame == 1){
+    // line
+    strokeWeight(1); 
+    stroke(255); 
+    line(width/2,10,width/2,height-10);
+
+    // ball
+    if(revealBall){
+      rect(ballX, ballY, 35, 35);
+      lightOn.play(); 
+      revealBall = false; 
+    }
+    if(ballColor > 0){
+      ballTimer ++;
+			ballColor -=0.43; 
+    }
+    if(ballTimer == 480){ // 8 seconds due to fr of 60f/s
+       ballColor = 0;
+       ballTimer = 0; 
+       lightOff.play();
+    }
+    
+    fill(255,255,255,ballColor);
+    noStroke();
+    rect(ballX, ballY, ballWidth, ballHeight);
+
+    // paddles
+		paddleMoving.setVolume(0.3);
+		fill(255,255,255,paddleColor);
+    if(revealPaddle){
+      rect(paddle1X, paddle1Y, paddleWidth+10, paddleHeight+10);
+			rect(paddle2X, paddle2Y, paddleWidth+10, paddleHeight+10);
+			lightOn.play(); 
+      revealPaddle = false; 
+    }
+    if(paddleColor > 0){
+      paddleTimer ++;
+			paddleColor -= 0.24; 
+    }
+    if(paddleTimer == 900){ // 15 seconds due to fr of 60f/s
+       paddleColor = 0;
+       paddleTimer = 0; 
+       lightOff.play();
+    }
+
+    rect(paddle1X, paddle1Y, paddleWidth, paddleHeight); //p1
+    rect(paddle2X, paddle2Y, paddleWidth, paddleHeight); //p2
+    
+    // walls
+    fill(255); 
+    rect(width/2,10,width,7);
+    rect(width/2,490,width,7);  
+
+    // physics
+    ballX = ballX + ballDirectionX * ballXSpeed; 
+    ballY = ballY + ballDirectionY * ballYSpeed; 
+
+    // collisions
+    wallHit.setVolume(3); 
+    if(ballY >= height-20){ // bottom wall collision
+       ballDirectionY = ballDirectionY * -1;
+       wallHit.play();
+       rect(width/2,490,width,15);  
+    }
+    if(ballY <= 20){ // top wall collision
+       ballDirectionY = ballDirectionY * -1;
+       wallHit.play();
+       rect(width/2,10,width,15);
+    } 
+
+    paddleHit.setVolume(3);
+    // right paddle collision
+    if(ballX >= paddle2X - 6 && ballX <= paddle2X + 6){
+      if(ballY <= paddle2Y + 41 && ballY >= paddle2Y - 41){
+
+        if(ballY >= paddle1Y-41 && ballY <= paddle1Y-20){
+          ballYSpeed *=1.5; 
+        }
+        else if(ballY <= paddle1Y + 41 && ballY >= paddle1Y + 20){
+          ballYSpeed *=1.5; 
+        }
+        else if (ballYSpeed > 2){
+          ballYSpeed *= 0.9; 
+        }
+        else{
+          ballYSpeed = 2; 
+        }
+
+        ballDirectionX = ballDirectionX * -1;
+        ballXSpeed += 0.2; 
+        paddleHit.play();
+      }
+    }
+    // left paddle collision 
+    if(ballX >= paddle1X - 6 && ballX <= paddle1X + 6){
+      if(ballY <= paddle1Y + 41 && ballY >= paddle1Y - 41){
+
+        if(ballY >= paddle1Y-41 && ballY <= paddle1Y-20){
+          ballYSpeed *=1.5; 
+        }
+        else if(ballY <= paddle1Y + 41 && ballY >= paddle1Y + 20){
+          ballYSpeed *=1.5; 
+        }
+        else if (ballYSpeed > 2){
+          ballYSpeed *= 0.9; 
+        }
+        else{
+          ballYSpeed = 2; 
+        }
+
+        ballDirectionX = ballDirectionX * -1;
+        ballXSpeed += 0.2;
+        paddleHit.play(); 
+      }
+    }
+
+    // scores
+    textFont("Courier New");
+    textSize(50); 
+    text(p1Score, width/2-60, 70);
+    text(p2Score, width/2+60, 70);
+
+    if(ballX >= width){ // player1 scores
+      p1Score ++;
+      ballX = width/2;
+      ballY = height/2;
+      ballXSpeed = 2;
+      ballYSpeed = 2;
+      goalNoise.play(); 
+    }
+    if(ballX <= 0){ // player 2 scores
+      p2Score ++;
+      ballX = width/2;
+      ballY = height/2;
+      ballXSpeed = 2; 
+      ballYSpeed = 2;
+      goalNoise.play();
+    }
+
+    textSize(20);
+    text("P1", 50,50);
+    text("CPU", width-50,50);
+
+    // computer player
+    if(paddle2Y > ballY && paddle2Y > 50){
+       paddle2Y -= 2.3; 
+    }
+    if(paddle2Y < ballY && paddle2Y < height - 50){
+       paddle2Y += 2.3; 
+    }
+  }
+} // close draw
+
+function keyTyped(){
+  if(key == 's' && keyIsPressed){
+     ballColor = 255;
+		 ballTimer = 0; 
+     revealBall = true; 
+  }
+  if(key == 'a' && keyIsPressed){
+     paddleColor = 255;
+		 paddleTimer = 0; 
+     revealPaddle = true; 
+  }
+}
+
+function keyPressed(){
+  if(key == "o" && paddle1Y > 50 && keyIsPressed){
+     paddle1Y = paddle1Y - playerSpeed;
+		 paddleMovingDown = false; 
+		 if(paddleMovingUp == false){
+		 paddleMoving.play();
+		 paddleMovingUp = true;
+		 }
+  }
+  if(key == "l" && paddle1Y < height - 50 && keyIsPressed){
+		 paddle1Y = paddle1Y + playerSpeed;
+		 paddleMovingUp = false;
+		 if(paddleMovingDown == false){
+		 paddleMoving.play();
+		 paddleMovingDown = true; 
+		 }
+  }
+  if(keyCode == SHIFT && keyIsPressed){
+     ballX = width/2;
+     ballY = height/2;
+     ballXSpeed = 2;
+     ballYSpeed = 2;
+     p1Score = 0;
+     p2Score = 0;
+     isPlayingGame = 0; 
+  }
+  if(keyCode == ENTER && keyIsPressed){
+    isPlayingGame = 1; 
+  }
+}
